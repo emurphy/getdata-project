d1fcd3d0d08d60cb2029dc805d805562fb31d94b
Emmet Murphy <emmet@emurphy.net>
2016-01-17 23:51:32 -0800

    merge the test and train data sets, with activity labels and subjects (steps 1-4)

--- run_analysis.R
+++ run_analysis.R
@@ -5,4 +5,36 @@ if (!file.exists("UCI HAR Dataset")) {
     unzip("UCI-HAR-dataset.zip")
 }
 
+# read in datasets
+features <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", header=FALSE)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", header=FALSE)
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", header=FALSE)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
 
+# set column names from features
+colnames(x_test) <- features$V2
+colnames(x_train) <- features$V2
+
+# add activity labels
+y_test_labeled <- merge(y_test, activity_labels, by.x = "V1", by.y = "V1")
+y_train_labeled <- merge(y_train, activity_labels, by.x = "V1", by.y = "V1")
+x_test <- cbind(x_test, activity = y_test_labeled$V2)
+x_train <- cbind(x_train, activity = y_train_labeled$V2)
+
+# add subjects
+x_test <- cbind(x_test, subject = subject_test$V1)
+x_train <- cbind(x_train, subject = subject_train$V1)
+
+# merge test and train, adding a new variable for sampleType
+x_test$sampleType <- "test"
+x_train$sampleType <- "train"
+x_merged <- rbind(x_test, x_train)
+
+# extract mean and std columns
+x_select <- subset(x_merged, select=(names(x_merged)[grep('-mean()|-std()|^activity|^sampleType|^subject',names(x_merged))]))
+
+# todo: average each variable for each activity and each subject
